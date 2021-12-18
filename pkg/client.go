b32a75d0548f7aea9ab11bf9ff0dc9506ec8a6c4
George Malamidis <george@loveholidays.com>
2021-12-18 13:34:48 +0000

    Added license blurb to source code files. Read and throw away HTTP response body.

--- pkg/client.go
+++ pkg/client.go
@@ -1,6 +1,25 @@
+/*
+ripley
+Copyright (C) 2021  loveholidays
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package ripley
 
 import (
+	"io"
 	"net/http"
 	"time"
 )
@@ -36,6 +55,14 @@ func doHttpRequest(client *http.Client, requests <-chan *request, results chan<-
 		}
 
 		resp, err := client.Do(httpReq)
+
+		if err != nil {
+			results <- &result{err: err}
+			return
+		}
+
+		_, err = io.ReadAll(resp.Body)
+		defer resp.Body.Close()
 
 		if err != nil {
 			results <- &result{err: err}
