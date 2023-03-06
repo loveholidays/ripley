89c0b6990ef3bfc8c0f19e7a7f775b15d8687f14
Eugene Paniot <e.paniot@gmail.com>
2023-03-06 20:01:11 +0000

    Rewrite dummyweb.go using fasthttp

--- etc/dummyweb.go
+++ etc/dummyweb.go
@@ -1,23 +1,97 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
-	"net/http"
-	"net/http/httputil"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/reuseport"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf("%v: %s\n", time.Now().Format(time.UnixDate), string(dump))
-	w.Write([]byte("hi\n"))
-}
+var silent bool
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	flag.BoolVar(&silent, "silent", false, "whether to silence output")
+	addr := flag.String("addr", "localhost:8080", "server listen address. Default: localhost:8080")
+	flag.Parse()
+
+	// Create a new listener on the given address using port reuse
+	ln, err := reuseport.Listen("tcp4", *addr)
+	if err != nil {
+		log.Fatalf("error creating listener: %v", err)
+	}
+	defer ln.Close()
+
+	// Create a new fasthttp server
+	server := &fasthttp.Server{
+		TCPKeepalive:    true,
+		LogAllErrors:    true,
+		ReadBufferSize:  1024 * 1024,
+		WriteBufferSize: 1024 * 1024,
+		Handler:         requestHandler,
+	}
+
+	// Start the server in a goroutine
+	go func() {
+		if err := server.Serve(ln); err != nil {
+			log.Fatalf("error starting server: %v", err)
+		}
+	}()
+
+	// Wait for a signal to stop the server
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+
+	// Stop the server
+	server.Shutdown()
+}
+
+func requestToJSON(req *fasthttp.Request) ([]byte, error) {
+	type requestJSON struct {
+		URI         string            `json:"uri"`
+		Method      string            `json:"method"`
+		Headers     map[string]string `json:"headers"`
+		ContentType string            `json:"content_type"`
+		Body        string            `json:"body"`
+	}
+
+	// Get the request URI, method, headers, content type, and body
+	uri := string(req.URI().FullURI())
+	method := string(req.Header.Method())
+	headers := make(map[string]string)
+	req.Header.VisitAll(func(k, v []byte) {
+		headers[string(k)] = string(v)
+	})
+	contentType := string(req.Header.ContentType())
+	body := string(req.Body())
+
+	// Create a requestJSON struct and marshal it to JSON
+	reqJSON := &requestJSON{
+		URI:         uri,
+		Method:      method,
+		Headers:     headers,
+		ContentType: contentType,
+		Body:        body,
+	}
+	return json.Marshal(reqJSON)
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	jsonData, _ := requestToJSON(&ctx.Request)
+
+	if !silent {
+		fmt.Println(string(jsonData))
+	}
+
+	ctx.SetContentType("application/json")
+	ctx.Response.Header.SetContentLength(len(jsonData))
+	ctx.Response.Header.Set("Connection", "keep-alive")
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.Write(jsonData)
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 )
